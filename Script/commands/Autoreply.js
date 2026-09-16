4e22a2adc39ac0c216792ad18b6e0fb36aa00cd6
eramhasan275-ui <eramhasan275@gmail.com>
2026-09-16 14:40:49 +0600

    Update Autoreply.js

--- Script/commands/Autoreply.js
+++ Script/commands/Autoreply.js
@@ -43,7 +43,7 @@ module.exports.handleEvent = async function ({ api, event }) {
     "sadiya": "সাদিয়া কে ডেকো না ফুস করে দিবে",
     "sabab1": "সাবাব, বেবী কাছে আসো 🙄",
 
-    ";⁠)": "rongless happy guy ;⁠)",
+    ":)": "rongless happy guy ;⁠)",
 
     "faraz 1": "ফারাজ ভদ্র ছেলে না তেলে না ঝোলে 🙉",
 
